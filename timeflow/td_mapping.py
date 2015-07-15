ab2595e171364b21828fce089a6d43aede2c47b9
Andrew Lee <ajylee@gmail.com>
2015-07-14 18:24:09 -0700

    Fix import for `StepMapping.at_time`

--- timeflow/td_mapping.py
+++ timeflow/td_mapping.py
@@ -2,7 +2,7 @@ import collections
 import itertools
 import toolz as tz
 from operator import ne
-from .base import BaseTimeLine, TimeLine, DerivedObject, DerivedStage, new_timeflow_id
+from .base import BaseTimeLine, TimeLine, DerivedObject, DerivedStage, new_timeflow_id, now
 
 delete = ('delete', object)
 no_element = ('no_element', object)
