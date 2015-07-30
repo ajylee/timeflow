6d53f6838e4eab2b2862ca8bb2c9bd5a18c653a5
Andrew Lee <ajylee@gmail.com>
2015-07-30 11:35:38 -0700

    Fixup

--- tests/test_td_mapping.py
+++ tests/test_td_mapping.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 import nose.tools
 
 class Repo(object):
-    def __init__(self)
+    def __init__(self):
         self.current_branch = 'master'
         self.refs = {'HEAD': None,
                   self.current_branch: None}
@@ -13,22 +13,22 @@ class Repo(object):
     def HEAD(self):
         return self.refs['HEAD']
 
-    def new_root_event():
-        assert not self.event_map
+    def new_root_event(self):
+        assert not self.refs['HEAD'] or self.refs[self.current_branch]
         _e = Event(parent=None)
-        self.refs[current_branch] = _e
+        self.refs[self.current_branch] = _e
         self.refs['HEAD'] = _e
         return _e
 
-    def commit(plan):
+    def commit(self, plan):
         event = Event(parent=plan.base_time)
         plan.commit(event)
         self.refs[self.current_branch] = event
         return event
 
-    def new_plan(timelines):
+    def new_plan(self, timelines):
         base_event = self.HEAD
-        plan = Plan([tl])
+        return Plan(timelines, base_event)
 
 
 class TestData:
@@ -62,6 +62,7 @@ def test_td_mapping_2():
 
 
 def test_step_mapping():
+    repo = Repo()
     original = TestData.original
     sm = StepMapping(original.copy())
 
@@ -72,7 +73,7 @@ def test_step_mapping():
     del sm.at(plan)['to_delete']
 
     assert sm.head == original
-    commit(plan)
+    repo.commit(plan)
     assert sm.head == {'a':30, 'b':20, 'new':100}
 
 
