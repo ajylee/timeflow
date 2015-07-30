2476f2f2654570fff1ea19e9b48fa0ea62a69b22
Andrew Lee <ajylee@gmail.com>
2015-07-30 08:25:15 -0700

    Use Repo to manage events in test_td_mapping

--- tests/test_td_mapping.py
+++ tests/test_td_mapping.py
@@ -3,10 +3,32 @@ from timeflow import TimeLine, now, Event
 from collections import OrderedDict
 import nose.tools
 
-def commit(plan):
-    event = Event(parent=plan.base_time)
-    plan.commit(event)
-    return event
+class Repo(object):
+    def __init__(self)
+        self.current_branch = 'master'
+        self.refs = {'HEAD': None,
+                  self.current_branch: None}
+
+    @property
+    def HEAD(self):
+        return self.refs['HEAD']
+
+    def new_root_event():
+        assert not self.event_map
+        _e = Event(parent=None)
+        self.refs[current_branch] = _e
+        self.refs['HEAD'] = _e
+        return _e
+
+    def commit(plan):
+        event = Event(parent=plan.base_time)
+        plan.commit(event)
+        self.refs[self.current_branch] = event
+        return event
+
+    def new_plan(timelines):
+        base_event = self.HEAD
+        plan = Plan([tl])
 
 
 class TestData:
@@ -14,11 +36,14 @@ class TestData:
 
 
 def test_td_mapping_2():
-    original = TestData.original
-    tl = TimeLine({Event(): SnapshotMapping(original)})
+    repo = Repo()
 
-    e0 = tl.events[0]
-    plan = Plan([tl], e0)
+    original = TestData.original
+
+    e0 = repo.new_root_event()
+    tl = TimeLine({e0: SnapshotMapping(original)})
+
+    plan = repo.new_plan([tl])
 
     tl.at(plan)['a'] = 30
     tl.at(plan)['b'] = 2 * tl.at(plan)['a']
@@ -27,7 +52,7 @@ def test_td_mapping_2():
     assert tl.at(plan)._base == tl.at(e0)
     assert tl.at(e0) == original
 
-    e1 = commit(plan)
+    e1 = repo.commit(plan)
 
     assert tl.at(e0) == original
     assert tl.at(e1) == dict(a=30, b=60)
@@ -47,7 +72,7 @@ def test_step_mapping():
     del sm.at(plan)['to_delete']
 
     assert sm.head == original
-    plan.commit()
+    commit(plan)
     assert sm.head == {'a':30, 'b':20, 'new':100}
 
 
