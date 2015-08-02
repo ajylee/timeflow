097937b06248642ef737026db66e04e4320a9201
Andrew Lee <ajylee@gmail.com>
2015-08-01 23:32:11 -0700

    Copy over parent instances when creating event

--- timeflow/event.py
+++ timeflow/event.py
@@ -12,58 +12,6 @@ def strong_ref(xx):
 empty_ref = strong_ref(None)
 
 
-class Event(object):
-    def __init__(self, parent=None):
-        self._parent = strong_ref(parent) if parent else empty_ref
-        if parent:
-            parent._child = weakref.ref(self)
-
-        self.instance = {}
-
-        self._child = empty_ref
-        self.time = _time.time()
-
-        if parent and parent.time == self.time:
-            self.count = parent.count + 1
-        else:
-            self.count = 0
-
-    def __hash__(self):
-        return hash((self.time, self.count))
-
-    def __cmp__(self, other):
-        return cmp((self.time, self.count), (other.time, other.count))
-
-    def parent(self, nn=1):
-        target = self
-        while nn > 0:
-            nn = nn - 1
-            target = target._parent()
-            if not target:
-                return NullEvent
-
-        return target
-
-    def child(self, nn=1):
-        target = self
-        while nn > 0:
-            nn = nn - 1
-            target = target._child()
-            if not target:
-                raise ValueError, 'No child'
-
-        return target
-
-    def new_child(self):
-        return Event(parent=self)
-
-    def __repr__(self):
-        return 'Event(time={self.time}, count={self.count})'.format(self=self)
-
-    def forget_parent(self):
-        self._parent = empty_ref
-
-
 def representative_event(events, event):
     """If event is not in the list of events, return the event recorded just before."""
     index = bisect.bisect_right(events, event)
@@ -119,3 +67,56 @@ _neg_inf = _NegativeInfinity()
 class NullEvent:
     instance = {}
     time = -inf
+    count = 0
+
+
+class Event(object):
+    def __init__(self, parent=NullEvent):
+        self._parent = strong_ref(parent) if parent else empty_ref
+        if parent:
+            parent._child = weakref.ref(self)
+
+        self.instance = ({} if parent is None or parent is NullEvent else parent.instance.copy())
+
+        self._child = empty_ref
+        self.time = _time.time()
+
+        if parent and parent.time == self.time:
+            self.count = parent.count + 1
+        else:
+            self.count = 0
+
+    def __hash__(self):
+        return hash((self.time, self.count))
+
+    def __cmp__(self, other):
+        return cmp((self.time, self.count), (other.time, other.count))
+
+    def parent(self, nn=1):
+        target = self
+        while nn > 0:
+            nn = nn - 1
+            target = target._parent()
+            if not target:
+                return NullEvent
+
+        return target
+
+    def child(self, nn=1):
+        target = self
+        while nn > 0:
+            nn = nn - 1
+            target = target._child()
+            if not target:
+                raise ValueError, 'No child'
+
+        return target
+
+    def new_child(self):
+        return Event(parent=self)
+
+    def __repr__(self):
+        return 'Event(time={self.time}, count={self.count})'.format(self=self)
+
+    def forget_parent(self):
+        self._parent = empty_ref
