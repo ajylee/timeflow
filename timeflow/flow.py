7ae89785000ec09af9741f48067fa1b74506fcef
Andrew Lee <ajylee@gmail.com>
2016-04-30 00:14:12 -0700

    Reinstate Flow.introduce_at

--- timeflow/flow.py
+++ timeflow/flow.py
@@ -2,6 +2,12 @@
 class Flow(object):
     default = 'need override'
 
+    @classmethod
+    def introduce_at(cls, plan, snapshot_or_stage):
+        _flow = cls()
+        _flow.at(plan).update(snapshot_or_stage)
+        return _flow
+
     def __hash__(self):
         return object.__hash__(self)
 
