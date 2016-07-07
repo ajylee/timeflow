b83419ea208558395dd41c5d41ade7337308ebb9
Andrew Lee <ajylee@gmail.com>
2016-07-06 23:28:28 -0700

    Cover case of "fork" with no fork topology
    When deleting a TimeLine where there is a TimeLine with HEAD at an
    ancestor event, ensure that the relevant linked_structures are weakly
    linked to their bases.
    
    (TODO: fix "walk_to_fork" terminology. Maybe replace with
    "walk_to_fork_or_head".)

--- timeflow/timeline.py
+++ timeflow/timeline.py
@@ -13,6 +13,15 @@ from .linked_structure import (LinkedStructure, transfer_core, walk_to_core,
 
 now = ('now', uuid.UUID('5e625fb4-7574-4720-bb91-3a598d2332bd'))
 
+def non_independent_linked_structures(event):
+    for _elt in toolz.cons(event.instance, event.instance.itervalues()):
+        try:
+            if _elt.relation_to_base != SELF:
+                yield _elt
+        except AttributeError:
+            pass
+
+
 class TimeLine(object):
     def __init__(self, HEAD=None):
         self.HEAD = HEAD if HEAD is not None else NullEvent()
@@ -46,6 +55,14 @@ class TimeLine(object):
 
         for _event, _parent in toolz.sliding_window(2, path_to_fork):
             _parent.referrers = _drop_from_tuple(_parent.referrers, _event)
+
+        if has_fork:
+            # covers case where there is no fork topology; i.e. a branch has
+            # been started but no fork commit has been made.
+            fork = path_to_fork[-1]
+            for _elt in non_independent_linked_structures(fork):
+                if type(_elt.base) is not weakref.ProxyType:
+                    _parent.base = weakref.proxy(_parent.base)
 
         #if has_fork:
         #    fork = path_to_fork[-1]
